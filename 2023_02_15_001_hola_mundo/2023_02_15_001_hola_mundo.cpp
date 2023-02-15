13e818372a555018b2b9772363ae130db9ef409d
Jorgecactus <119530716+tadeusaurus@users.noreply.github.com>
2023-02-15 09:37:30 -0600

    001_hola mundo
    esta es la practica numero 01 con el nombre hola mundo

---
+++ 2023_02_15_001_hola_mundo/2023_02_15_001_hola_mundo.cpp
@@ -0,0 +1,11 @@
+// 2023_02_15_001_hola_mundo.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "tadeo!\n";
+    std::cout << "tachanka \n";
+}
+
